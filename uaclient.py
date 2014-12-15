8f01db6eac978bc99aa98eac1607bcce68cf8221
Ubuntu-14.04.1 <a.oterinoc@alumnos.urjc.es>
2014-12-15 19:24:43 +0100

    diccionario UA1 a falta de log

--- uaclient.py
+++ uaclient.py
@@ -14,7 +14,7 @@ import os
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-        self.etiquetas = []
+        self.diccionario = {}
         self.tags = ["account", "uaserver", "rtpaudio", "regproxy", "log", "audio"]
         self.atributos = {
             "account": ["username"],
@@ -29,16 +29,14 @@ class SmallSMILHandler(ContentHandler):
         """
         Método que se emplea al abrirse una etiqueta
         """
-
-        self.diccionario = {}
+        
         if name in self.tags:
-            self.diccionario['etiqueta'] = name
             for atributo in self.atributos[name]:
-                self.diccionario[atributo] = attrs.get(atributo, "")
-            self.etiquetas.append(self.diccionario)
+                elem = name + "_" + atributo
+                self.diccionario[elem] = attrs.get(atributo, "")
 
     def get_tags(self):
-        return self.etiquetas
+        return self.diccionario
 
 if __name__ == "__main__":
     # Dirección IP, puerto y mensaje para servidor
@@ -46,47 +44,71 @@ if __name__ == "__main__":
         print "Usage : uaclient.py config method option"
         sys.exit()
     CONFIG_XML = sys.argv[1]
-    METODO = sys.argv[2]
-    #OPCION cambiara segun el metodo empleado
+    metodo = sys.argv[2]
+    METODO = metodo.upper()
+    #OPCION[3] cambiara segun el metodo empleado
      
-    #parte del programa smallsmilhander para parsear el xml
+    #parte del programa smallsmilhander para parsear el XML
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open(CONFIG_XML))
     diccionario = cHandler.get_tags()
 
-    #tengo que crear el log para ir registrando los distintos metodos
-
-    usuario = diccionario['username']
-    if METODO == "REGISTER":
-    # REGISTER sip:luke@polismassa.com SIP/2.0\r\n\r\n
-        OPCION_EXPIRES = sys.argv[3]
-        LINE = METODO.upper() + " sip:" + usuario + " SIP/2.0\r\n"
-        LINE = LINE + "Expires: " + str(T_EXPI) + "\r\n\r\n"
-
-    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #my_socket.connect((193.147.73, 5555)) #!!!!!!!!!!!!!
+    #extraigo informacion XML
+    usuario = diccionario['account_username']
+    puerto = int(diccionario['uaserver_puerto'])
+    ip = diccionario['uaserver_ip']
+    puerto_rtp = int(diccionario['rtpaudio_puerto'])
+    ip_proxy = diccionario['regproxy_ip']
+    puerto_proxy = diccionario['regproxy_puerto']
     
+    #tengo que crear el log para ir registrando los distintos metodos!!!
+    #fichero
+    #def log()
+    fich_log = lista['log_path']
+    fich = open(fich_log, "a")
+
+    tiempo = time.time()
+    hora = time.strftime('%Y-%m-%d %H:%M:%S', tiempo)
+    proxy = ip_proxy + ":" + puerto_proxy
+    fich.write(hora + "\t" + "accion que se ejecuta" + "\t" + proxy + "\r\n")
+    print diccionario
+
+    
+    
+    if METODO == "REGISTER":
+    # REGISTER sip:luke@polismassa.com:puerto SIP/2.0\r\n
+        OPCION_EXPIRES = sys.argv[3]
+        LINE = METODO.upper() + " sip:" + usuario + ":" + str(puerto) + " SIP/2.0\r\n"
+        LINE = LINE + "Expires: " + str(OPCION_EXPIRES) + "\r\n\r\n"
+        print LINE
     elif METODO == "INVITE":
-        #INVITE sip:receptor SIP/2.0\r\n\r\n
+        #INVITE sip:receptor SIP/2.0\r\n
         OPCION_RECEPTOR = sys.argv[3] #la opcion es a quien se lo mandamos
-        LINE = "INVITE sip:" + OPCION_RECEPTO + " SIP/2.0\r\n"
+        LINE = "INVITE sip:" + OPCION_RECEPTOR + " SIP/2.0\r\n"
         LINE += "ContentType: application/sdp" + "\r\n\r\n"
         LINE += "v=0" + "\r\n"
-        LINE += "o=" + name + " " + ip + "\r\n"
+        LINE += "o=" + usuario + " " + ip + "\r\n"
         LINE += "s= misesion" + "\r\n"
         LINE += "t=0" + "\r\n"
-        LINE += "m= audio " + puerto_rtp + " RTP" + "\r\n"
+        LINE += "m= audio " + str(puerto_rtp) + " RTP" + "\r\n"  
+        print LINE
     elif METODO == "BYE":
         OPCION_RECEPTOR = sys.argv[3]
         LINE = "BYE sip:" + OPCION_RECEPTOR + " SIP/2.0\r\n"
-    print usuario
-    print LINE
-    print "Enviando: " + LINE
-    my_socket.send(LINE + '\r\n')
+        print LINE
+    else:  #SI NO ES NINGUNO DE ESTOS METODOS
+        sys.exit("Usage: python uaclient.py config method opcion")
+    
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    #me tengo que atar al proxy porque es con el que intercambio datos
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((ip_proxy, int(puerto_proxy)))
+    
+    print "Envio: " + LINE
+    my_socket.send(LINE)
 
     #Excepcion en caso de establecer conexion con un puerto no abierto
     try:
@@ -96,11 +118,12 @@ if __name__ == "__main__":
 
 
     except (socket.error):
-        print "No server listening at " + str(SERVER) + " port " + str(PORT)
+        print "No server listening at " + str(ip_proxy) + " port " + str(puerto_registrar)
         sys.exit()
 
     print "Terminando socket..."
 
     # Cerramos todo
     my_socket.close()
+    
     print "Fin."
