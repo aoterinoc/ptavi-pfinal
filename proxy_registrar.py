d10a106d33260cf65ab064c75a123878edc7ef4c
Ubuntu-14.04.1 <a.oterinoc@alumnos.urjc.es>
2014-12-18 18:53:18 +0100

    proxy/reg registro, register,invite

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -12,8 +12,9 @@ from xml.sax.handler import ContentHandler
 import os
 import SocketServer
 
+
 registro={}
-METODOS_ACEPTADOS = ["INVITE", "BYE", "ACK"]
+METODOS_ACEPTADOS = ["REGISTER" , "INVITE", "BYE", "ACK"]
 
 class ProxyHandler(ContentHandler):
 
@@ -21,7 +22,7 @@ class ProxyHandler(ContentHandler):
         self.diccionario = {}
         self.tags = ["server", "database", "log"]
         self.atributos = {
-            "server": ["name","ip"],
+            "server": ["name","ip", "puerto"],
             "database": ["path"],
             "log": ["path"],
         }
@@ -38,39 +39,48 @@ class ProxyHandler(ContentHandler):
 
     def get_tags(self):
         return self.diccionario
-    
-    #REGISTRO de USUARIOS
-    """
-    def register2file(self):
-        
-        Escribe en un fichero los usuarios registrados o dados de baja
-        con sus valores.
-        
 
-        fich = open("registered.txt", "w")
-        fich.write("User" + "\t" + "IP" + "\t" + "Expires" + "\r\n")
-        for clave in registro:
-            tiempo = time.gmtime(registro[clave][2])
-            hora = time.strftime('%Y-%m-%d %H:%M:%S', tiempo)
-            fich.write(clave+"\t"+registro[clave][0]+"\t"+hora+"\r\n")
-        fich.close()
-       """
-       #BORRO USUARIOS CADUCADOS
-
-        
-            
-#Implementación codigos de respuesta
-
+  
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
+    #---------BASE DE DATOS---------
     
+    def register2file(self):
+        """
+        Listado con los usuarios registrados en cada momento.
+        """
 
+        fich = open("registered.txt", "w")
+        escribe = "User" + "\t" + "IP" + "\t" + "Port" + "\t"
+        escribe += "Date" + "\t" + "Expires" + "\r\n"
+        fich.write(escribe)
+        for clave in registro:
+            tiempo = time.gmtime(float(registro[clave][2]))
+            hora = time.strftime('%Y%m%d%H%M%S', tiempo)
+            linea = clave + "\t" + "IP!!" + "\t" + registro[clave][2] + "\t"
+            linea += hora + "\t" + str(registro[clave][4]) + "\r\n"
+            fich.write(linea)
+        fich.close()
+    
+    def borrar_caducados(self, registro):
+        """
+        Gestiona la caducidad de los usuarios registrados
+        """
+        list_caducados = []
+        for clave in registro:
+            hora_entrada = registro[clave][3]
+            expires = registro[clave][4]
+            tiemp_a = time.time()
+            if int(hora_entrada) + int(expires) <= tiemp_a:
+                list_caducados.append(clave)
+        for clave in list_caducados:
+            del registro[clave]
+    
     def handle(self):
-       
-        
-        while 1:
+
+       while 1:
             # Lee linea a lina lo que llega del cliente
 
             line = self.rfile.read()
@@ -80,64 +90,86 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
             print "Recibo del cliente"
             print line
+            
+            """
+            ¿COMO COMPRUEBO SI EL MENSAJE ESTA BIEN FORMADO?
             if line != "":
                 sip = "SIP/2.0\r\n\r\n"
                 #miro aqui el codigo 400 xq el split me corta los \r\n
                 if 'sip:' not in line or sip not in line or "@" not in line:
                     self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
                     break
-                    #Si el cliente me manda mal el mensaje no le mando nada mas
-                #troceamos la linea que nos llega
-                line = line.split()
-                if line[0] == "REGISTER":
-                    self.wfile.write(" SIP/2.0 200 OK\r\n\r\n")
-                line[1] = line[1].split(":")
-                
-                #Solo metemos en el diccionario si el expires no es 0
-                #Me quedo con el valor de expires
-                #if valor de expires != "0":
-                    #hora_s = time.time()
-                    #registro[line[1][1]] = [str(self.client_address[0]), line[4], hora_s]
-              #  else:
-                #Si un usuario se da de baja y esta en el registro, le borramos
-              #  if line[1][1] in registro:
-              #      del registro[line[1][1]]
-              #      self.wfile.write(" SIP2.0 200 OK\r\n\r\n")
-                #Borro usuarios caducados y escribimos en el fichero
-              #  self.borrar_caducados(registro)
-              #  self.register2file()
-            print registro
-            if line[0] == "INVITE":
-                trying = "SIP/2.0 100 Trying\r\n\r\n"
-                ringing = "SIP/2.0 180 Ringing\r\n\r\n"
-                ok = "SIP/2.0 200 OK\r\n\r\n"
-                #SDP que se manda con el 200 OK
-                sdp = "ContentType: application/sdp" + "\r\n\r\n"
-                sdp += "v=0" + "\r\n"
-                nombre = diccionario['server_name']
-                ip = diccionario['server_ip']
-                sdp += "o=" + nombre + " " + ip + "\r\n"
-                sdp += "s= misesion" + "\r\n"
-                sdp += "t=0" + "\r\n"
-                cod_respuesta = trying + ringing + ok + sdp
-                self.wfile.write(cod_respuesta)
+            """
+            linea = line.split('\r\n')
+            print linea
             
-            if line[0] not in METODOS_ACEPTADOS:
+            #miro nombre metodo
+            lin = linea[0].split() 
+            metodo = lin[0]
+            print metodo
+            
+           
+            #COMPROBAR PETICION BIEN FORMADA!!!#
+            if metodo == "REGISTER":
+                linea = line.split('\r\n')
+                print linea
+                #saco valor expires
+                expires = linea
+                expires = linea[1].split(':')
+                expires = int(expires[1])
+                print expires
+                linea = linea[0].split() 
+                direcc = linea[1]
+                direcc = direcc.split(':')
+                puerto = direcc[2]
+                print puerto
+                direcc = direcc[1]
+                print direcc
+                
+                self.wfile.write(" SIP/2.0 200 OK\r\n\r\n")                
+            #Solo metemos en el diccionario si el expires no es 0
+                if expires != "0":
+                    hora_s = time.time()
+                    registro[direcc] = [direcc,"IP", puerto, hora_s, expires]
+                    #falta IP!!
+                if expires == "0":
+                #Si un usuario se da de baja y esta en el registro, le borramos
+                    if direcc in registro:
+                        del registro[direcc]
+                        print " se da de baja"                   
+                        self.wfile.write(" SIP2.0 200 OK\r\n\r\n")
+                #Borro usuarios caducados y escribimos en el fichero
+                #self.borrar_caducados(registro)
+                self.borrar_caducados(registro)
+                self.register2file()
+                print registro
+            
+            if metodo not in METODOS_ACEPTADOS:
                 self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
-            #404 USER NOT FOUND USUARIO NO REGISTRADO
-            if line[0] == "BYE":
-                self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-
-
-
-
-
-
-
-
-
+            
+            if metodo == "INVITE":
+                nom_client = linea[4].split('=')
+                nom_client = nom_client[1].split(' ')
+                nom_client = nom_client[0]
+                if nom_client in registro:
+                    trying = "SIP/2.0 100 Trying\r\n\r\n"
+                    ringing = "SIP/2.0 180 Ringing\r\n\r\n"
+                    ok = "SIP/2.0 200 OK\r\n\r\n"
+                    #SDP que se manda con el 200 OK
+                    sdp = "ContentType: application/sdp" + "\r\n\r\n"
+                    sdp += "v=0" + "\r\n"
+                    nombre = diccionario['server_name']
+                    ip = diccionario['server_ip']
+                    sdp += "o=" + nombre + " " + ip + "\r\n"
+                    sdp += "s= misesion" + "\r\n"
+                    sdp += "t=0" + "\r\n"
+                    cod_respuesta = trying + ringing + ok + sdp
+                    self.wfile.write(cod_respuesta)
+                    
+                else:
+                    self.wfile.write("SIP/2.0 404 User Not Found\r\n\r\n")
 if __name__ == "__main__":
-    # Dirección IP, puerto y mensaje para servidor
+   # Dirección IP, puerto y mensaje para servidor
     if len(sys.argv) != 2:
         print "Usage: python proxy_registrar.py config"
         sys.exit()
@@ -149,14 +181,14 @@ if __name__ == "__main__":
     parser.parse(open(FICH_XML))
     diccionario = cHandler.get_tags()
     
-    #puerto = int(diccionario['server_port'])
-    nombre = diccionario['server_name']
-    IP_SERV = diccionario['server_ip']
-    #PORT = int(diccionario['server_puerto'])
-    serv = SocketServer.UDPServer((IP_SERV, 2020), ProxyHandler)
-    print "Server " + nombre + " listening at port " + "PUERTO"  + "..."
-   
-    serv.serve_forever()
+    IP = diccionario['server_ip']
+    PUERTO = diccionario['server_puerto']
+    #Instanciamos EchoHandler
+    serv = SocketServer.UDPServer((IP, int(PUERTO)), EchoHandler)
+
+    print "Listening..."
+    serv.serve_forever()           
+
 
 
 
